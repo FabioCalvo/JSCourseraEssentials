39cb15e35b68c2e8ab6f56215505683b92b10628
fcalvo@hotmail.com <fcalvo@hotmail.com>
2025-06-30 15:10:22 -0400

    Ternary

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -8,6 +8,9 @@ let userMessage;
 let userType = "subscriber";
 let userCategory = "Unknown";
 
+
+let isAuthenticated = true;
+
 if (userRole === "admin") {
     accessLevel = "Full access granted";
 } else if (userRole === "manager") {
@@ -40,6 +43,10 @@ switch (userType) {
         userCategory = "Unknown";
 }
 
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
 console.log("Access Level:", accessLevel);
 console.log("User Message:", userMessage);
 console.log("User Category:", userCategory);
+
+console.log("Authentication Status:", authenticationStatus);
